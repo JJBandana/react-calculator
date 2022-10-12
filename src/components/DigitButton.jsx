c9eb54c4f078bfd049a28330e3b707a756909d70
Juan José Briasco <jj.briasco@gmail.com>
2022-10-12 19:18:19 -0300

    first commit

---
+++ src/components/DigitButton.jsx
@@ -0,0 +1,5 @@
+import { ACTIONS } from '../App'
+
+const DigitButton = ({dispatch, digit}) => <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: {digit} })}>{digit}</button>
+
+export default DigitButton
---
+++ src/components/OperationButton.jsx
@@ -0,0 +1,5 @@
+import { ACTIONS } from "../App";
+
+const OperationButton = ({ dispatch, operation }) => <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}>{operation}</button>
+
+export default OperationButton
